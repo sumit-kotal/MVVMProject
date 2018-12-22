1c3df963da72f6b6eafb65fc8833629a71327cb2
sumit-kotal <kotalsumit@gmail.com>
2018-12-22 15:48:11 +0530

    Final Commit

--- app/src/main/java/com/aztechz/projectbinaryveda/View/MainActivity.kt
+++ app/src/main/java/com/aztechz/projectbinaryveda/View/MainActivity.kt
@@ -6,12 +6,14 @@ import android.os.Bundle
 import android.support.design.widget.TabLayout
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.aztechz.projectbinaryveda.Adapters.MainSectionsAdapter
 import com.aztechz.projectbinaryveda.R
 import com.aztechz.projectbinaryveda.ViewModel.SeekerViewModel
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar.*
 
 
 class MainActivity : AppCompatActivity(),View.OnClickListener {
@@ -38,11 +40,23 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
             val tab_work = data?.workFunctions
             val tab_industry = data?.industries
 
+            var spinnerArray : MutableList<String>? = ArrayList()
+
+            for (i in 0 until data?.roles!!.size) {
+                spinnerArray?.add(data.roles!![i].name!!)
+            }
+
+            val spinnerArrayAdapter = ArrayAdapter<String>(this, R.layout.spinner_item, spinnerArray)
+            spinnerArrayAdapter.setDropDownViewResource(android.R.layout
+                    .simple_spinner_dropdown_item)
+            spinner.adapter = spinnerArrayAdapter
+
             tabText.adapter = MainSectionsAdapter(supportFragmentManager,tab_skill,tab_work,tab_industry)
+            tabText.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
             tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(tabText))
 
             Picasso.get()
-                    .load(data?.image)
+                    .load(data.image)
                     .placeholder(R.mipmap.ic_launcher)
                     .error(android.R.drawable.stat_notify_error)
                     .into(circleImageView)
